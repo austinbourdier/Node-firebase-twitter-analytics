2ab3d201c01531127e9b43a1a2a50595cdb63c9a
Austin Bourdier <austinbourdier@gmail.com>
2014-08-14 03:50:50 -0700

    rolledback

--- helpers.js
+++ helpers.js
@@ -1,88 +1,83 @@
 var dataRef = new Firebase("https://scorching-fire-1875.firebaseio.com/");
 dataRef.limit(20).on("child_added", importFromFirebase);
 function importFromFirebase(snapshot){
-	analyzeSentimentValue(snapshot.val());
+    analyzeSentimentValue(snapshot.val());
 };
 
 
 // Highcharts spline handler
 $(function () {
-	$(document).ready(function() {
-		Highcharts.setOptions({
-			global: {
-				useUTC: false
-			}
-		});
-		var chart;
-		$('#spline').highcharts({
-			chart: {
-				type: 'spline',
+    $(document).ready(function() {
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
+        var chart;
+        $('#spline').highcharts({
+            chart: {
+                type: 'spline',
           animation: Highcharts.svg, // don't animate in old IE
           marginRight: 10,
           events: {
-          	load: function() {
-
+            load: function() {
                 var series = this.series[0];
                 setInterval(function() {
-                // score
-                // console.log(dataRef['m']['Ha']['$']['o']['ea']['value']['o']['ea']['left']['right']['value']['F'])
-                // time
-                // console.log(dataRef['m']['Ha']['$']['o']['ea']['value']['o']['ea']['right']['value']['F'])
                 var x = (new Date()).getTime(), // current time
                 y = metrics.positivePerSecond-metrics.negativePerSecond;
                 series.addPoint([x, y], true, true);
-            }, 3000);
+              }, 3000);
             }
-        }
-    },
-    title: {
-       text: 'Twitter Sentiment Analysis'
-   },
-   xAxis: {
-       title: {
-          text: 'Time',
-      },
-      type: 'datetime',
-      tickPixelInterval: 150
-  },
-  yAxis: {
-   title: {
-      text: 'Sentiment Rate'
-  },
-  plotLines: [{
-      value: 0,
-      width: 1,
-      color: '#808080'
-  }]
-},
-tooltip: {
-   formatter: function() {
-      return '<b>'+ this.series.name +'</b><br/>'+
-      Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
-      Highcharts.numberFormat(this.y, 2);
-  }
-},
-legend: {
-   enabled: false
-},
-exporting: {
-   enabled: false
-},
-series: [{
-   name: 'Sentiment Rate',
-   data: (function() {
-      var data = [],
-      time = (new Date()).getTime(),
-      i;
-      for (i = -25; i <= 0; i++) {
-         data.push({
-            x: time + i * 1000,
-            y: 0
-        });
-     }
-     return data;
- })()
-}]
-});
-});
+          }
+        },
+        title: {
+            text: 'Twitter Sentiment Analysis'
+        },
+        xAxis: {
+            title: {
+                text: 'Time',
+            },
+            type: 'datetime',
+            tickPixelInterval: 150
+        },
+        yAxis: {
+            title: {
+                text: 'Sentiment Rate'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>'+ this.series.name +'</b><br/>'+
+                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                Highcharts.numberFormat(this.y, 2);
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+        series: [{
+            name: 'Sentiment Rate',
+            data: (function() {
+                var data = [],
+                time = (new Date()).getTime(),
+                i;
+                for (i = -10; i <= 0; i++) {
+                    data.push({
+                        x: time + i * 1000,
+                        y: 0
+                    });
+                }
+                return data;
+            })()
+        }]
+      });
+    });
 });
--- view.js
+++ view.js
@@ -1,22 +1,20 @@
 // update the metrics table, this occurs as every tweet come ins
 function updateViewMetrics(){
-	$("#positive").html(metrics.positiveTweetCount);
-	$("#neutral").html(metrics.neutralTweetCount);
-	$("#negative").html(metrics.negativeTweetCount);
-	$("#positivePerSecond").html(metrics.positivePerSecond);
-	$("#neutralPerSecond").html(metrics.neutralPerSecond);
-	$("#negativePerSecond").html(metrics.negativePerSecond);
+  $("#positive").html(metrics.positiveTweetCount);
+  $("#neutral").html(metrics.neutralTweetCount);
+  $("#negative").html(metrics.negativeTweetCount);
+  $("#positivePerSecond").html(metrics.positivePerSecond);
+  $("#neutralPerSecond").html(metrics.neutralPerSecond);
+  $("#negativePerSecond").html(metrics.negativePerSecond);
 }
 
 // update counter every 1 second
 setInterval(function(){
-	metrics.seconds++;
-	$("#seconds").html("Seconds: " + metrics.seconds);
+  metrics.seconds++;
+  $("#seconds").html("Seconds: " + metrics.seconds);
 },1000);
 
 // Insert tweet into stream container along with color for this tweet
 function insertTweet(new_tweet, color){
-	$("#tweets").append("<p style='color:" + color +"'>" + new_tweet.name +  "______________" + new_tweet.text + "</p>");
+  $("#tweets").append("<p style='color:" + color +"'>" + new_tweet.name +  "______________" + new_tweet.text + "</p>");
 };
-
-
